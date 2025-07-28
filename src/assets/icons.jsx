dd5bb5ae811cb85ae11a6233b6e17867de0988af
mukeshdhayabaran@gmail.com <mukeshdhayabaran@gmail.com>
2025-07-28 10:48:15 +0530

    Modifying main

---
+++ src/assets/icons.jsx
@@ -0,0 +1,7 @@
+import PersonIcon from "@mui/icons-material/Person";
+import EmailIcon from "@mui/icons-material/Email";
+import LockIcon from "@mui/icons-material/Lock";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+
+export {PersonIcon,EmailIcon,LockIcon,VisibilityIcon,VisibilityOffIcon};
